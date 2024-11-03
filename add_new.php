6545e06cc7a2c6313cb846c0455ef8790a043f4e
Frankam487 <fkamgang06@gmail.com>
2024-11-02 18:23:50 -0700

    cdn link added

--- add_new.php
+++ add_new.php
@@ -26,10 +26,12 @@ if ($result) {
 <link rel="stylesheet" href="style.css">
   <title>add task</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<!- font awsome->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
   
-<nav class="" ">
+<nav class="navbar navbar-light justify-content-center fs-3 mb-5" >
   php complete CREUD Application
 </nav>
 <div class="container">
--- db_config.php
+++ db_config.php
@@ -4,4 +4,4 @@ $conn = mysqli_connect("localhost", "root", "", "creud");
 
 if(!$conn){
 die("connexion echouee..." . mysqli_connect_error());
-}
+} 

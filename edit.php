9d726144e2eb348b167c655b21bd6f06ab04db3f
Frankam487 <fkamgang06@gmail.com>
2024-11-02 18:01:07 -0700

    edit file added

--- edit.php
+++ edit.php
@@ -11,7 +11,7 @@ if(isset($_POST['submit'])){
 $result = mysqli_query($conn, $sql);
 if ($result) {
   header("Location: index.php?msg=New record created successfully ");
-
+echo "ok";
 } else {
   echo "failed: " . mysqli_error($conn);
 }
@@ -29,43 +29,49 @@ if ($result) {
 </head>
 <body>
   
-<nav class="" ">
+<nav class="navbar navbar-light justify-cintent-center">
   php complete CREUD Application
 </nav>
 <div class="container">
   <div class="content">
-    <h3>Add New Userr</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    <h3>Edit Userr</h3>
+    <p>click sur edit apres avoir editer ton user</p>
   </div>
+  <?php
+  $id = $_GET['id'];
+  $sql = "SELECT * FROM `employer` WHERE id = $id LIMIT 1";
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_assoc($result);
+  ?>
   <div class="content2">
     <form action="" method="post">
       <div class="row">
-        <div class="col">
+        <div class="col">  
           <label for="">First Name:</label>
-          <input type="text" name="first_name">
+          <input type="text" value="<?php echo $row['first_name']?> " name="first_name">
         </div>
         <div class="col">
           <label for="">Last Name:</label>
-          <input type="text" name="last_name">
+          <input type="text" value="<?php echo $row['last_name']?> " name="last_name">
         </div>
       </div>
 
       <div class="result">
           <label for="">Email:</label>
-          <input type="email" name="email" >
+          <input type="email" value="<?php $row['email'] ?> " name="email" >
       </div>
       <div class="form-group"> 
         <label for="">Gender:</label> &nbsp;
-        <input type="radio" name="gender" id="male" value="male">
+        <input type="radio" name="gender" id="male" <?php echo ($row['gender'] == 'male')? "checked": "";?>>
          <label for="male">Male</label>
          &nbsp;
-        <input type="radio" name="gender" id="female" value="female">
+        <input type="radio" name="gender" value="female" id="female" <?php echo ($row['gender'] == 'female')? "checked": "";?>>
         <label for="female">Female</label>
       </div>
 
       <div class="btn">
-        <button type="submit" name="submit">Save</button>
-        <a href="index.php">Cancel</a>
+        <button type="submit" class="btn btn-success" name="submit">Editer</button>
+        <a href="./index.php">Cancel</a>
       </div>
     </form>
   </div>
